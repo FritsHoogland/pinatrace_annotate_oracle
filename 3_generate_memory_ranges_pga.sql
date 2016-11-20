3c6a6b49ec69c85cf82cc4ac44b1c950b11515e2
Frits Hoogland <frits.hoogland@gmail.com>
2016-11-20 16:06:58 +0000

    improved pga descriptions.

--- 3_generate_memory_ranges_pga.sql
+++ 3_generate_memory_ranges_pga.sql
@@ -1,4 +1,4 @@
 set head off pages 0 lines 200 trimout on trimspool on feed off sqlblanklines off
 spool memory_ranges_pga.csv
-select to_number(rawtohex(heap_descriptor),'xxxxxxxxxxxxxxxx')||'|'||(to_number(rawtohex(heap_descriptor),'xxxxxxxxxxxxxxxx')+bytes)||'|'||'pga'||'|'||category||', '||heap_name||', '||name from v$process_memory_detail;
+select to_number(rawtohex(heap_descriptor),'xxxxxxxxxxxxxxxx')||'|'||(to_number(rawtohex(heap_descriptor),'xxxxxxxxxxxxxxxx')+bytes)||'|'||'pga'||'|'||category||', '||name||', '||heap_name from v$process_memory_detail;
 spool off
